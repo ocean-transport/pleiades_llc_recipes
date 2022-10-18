c9aa17f82f5729a7ee7d956190cfa7736b5bb008
Cory Spencer Jones <csjone15@pfe21.nas.nasa.gov>
2022-10-18 09:17:21 -0700

    Added code for slicing i and j

--- python_cli_data_export/run_sing.sh
+++ python_cli_data_export/run_sing.sh
@@ -2,4 +2,4 @@
 
 module load singularity
 
-singularity shell --bind /nobackup:/nobackup --bind /nobackupp1:/nobackupp1 --bind /nobackupp17:/nobackupp17 --bind /nobackupp19:/nobackupp19 --bind /home6/dmenemen:/home6/dmenemen  /nobackup/dbalwada/notebook_pangeo.sif
+singularity shell --bind /nobackup:/nobackup --bind /nobackupp12:/nobackupp12 --bind /nobackupp1:/nobackupp1 --bind /nobackupp17:/nobackupp17 --bind /nobackupp19:/nobackupp19 --bind /home6/dmenemen:/home6/dmenemen  /nobackup/csjone15/notebook_pangeo.sif
